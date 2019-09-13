8e1195908e419ebfcdd2509ef02de306683c7e14
Ngo Thanh Tung <tungnt@softech.vn>
2019-09-13 18:38:28 +0700

    Update GOOGLE TRANSLATE API

---
+++ AspNetCore.API/Modules/AspNetCore.Modules.API/Controllers/GoogleController.cs
@@ -0,0 +1,90 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleTranslateAPI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace AspNetCore.Modules.BCM.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public partial class GoogleController : Controller
+    {
+
+        private static readonly GoogleTranslator Translator = new GoogleTranslator();
+
+        private readonly string connectionString;
+        private readonly IConfiguration configuration;
+        private readonly IHttpClientFactory httpClientFactory;
+
+        private readonly IMemoryCache memoryCache;
+
+        private IHostingEnvironment hostingEnvironment;
+
+        public GoogleController(IConfiguration configuration, IHttpClientFactory httpClientFactory, IMemoryCache memoryCache, IHostingEnvironment hostingEnvironment)
+        {
+            this.configuration = configuration;
+            this.connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            this.httpClientFactory = httpClientFactory;
+
+            this.hostingEnvironment = hostingEnvironment;
+
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [AllowAnonymous]
+        [EnableCors("Allow-All")]
+        public async Task<IActionResult> Post(ApiVersion apiVersion, [FromBody] dynamic body)
+        {
+            var text = body.text.Value as string;
+
+            Language english = Language.English; // or even this way
+            Language vietnamese = Language.Vietnamese; // or even this way
+
+            TranslationResult result = await Translator.TranslateAsync(text, english, vietnamese);
+
+            var response = new
+            {
+                ok = true,
+                result = new 
+                {
+                    result.OriginalText,
+                    result.OriginalTextTranscription,
+                    result.FragmentedTranslation,
+                    result.MergedTranslation,
+                    result.ExtraTranslations
+                }
+            };
+
+            return new OkObjectResult(response);
+        }
+
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [AllowAnonymous]
+        [EnableCors("Allow-All")]
+        public IActionResult Get()
+        {
+            var response = new
+            {
+                ok = true,
+            };
+
+            return new OkObjectResult(response);
+        }
+    }
+}
---
+++ AspNetCore.API/Modules/GoogleTranslateAPI/ITranslatable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleTranslateAPI
+{
+	public interface ITranslatable
+	{
+		string OriginalText { get; }
+		Language FromLanguage { get; }
+		Language ToLanguage { get; }
+	}
+}
